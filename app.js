9861924e204e5a23c3bdd9dcb86eaa91fa56a14f
leeblazek <leeblazek@gmail.com>
2021-08-03 16:11:07 -0600

    prot

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/secret', authenticateRequest, function(req, res) {
 	res.send('Congratulations, you are in a secret area!');
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 function obtainToken(req, res) {
 
